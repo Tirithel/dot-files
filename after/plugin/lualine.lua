845729844c672d2c39b2adedacc0d724468a55e2
Colin Moran <colin.mtech@gmail.com>
2022-12-22 22:51:34 -0500

    feat: adding initial nvim functionality

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,8 @@
+require('lualine').setup {
+	options = { 
+		-- theme = "ayu_dark",
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''}
+	}
+}
+
---
+++ lua/base/remap.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- moves visual block up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- collapses next line, keeps cursor in the same place
+vim.keymap.set("n", "J", "mzJ`z")
+-- keep cursor in the middle with half page jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- keep cursor in the middle with search terms
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- "Q" is the worst, kill it with fire
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
